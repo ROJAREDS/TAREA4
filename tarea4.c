a7dcd4731daf4d34396978cfc5eeba6d63357d79
ROJAREDS <jc.rojas29@uniandes.edu.co>
2017-07-27 14:15:00 -0500

    tarea terminada

--- tarea4.c
+++ tarea4.c
@@ -6,33 +6,43 @@
 
 #define filas 500
 #define columnas 744
-#define pi 3.1415
+double latitud(int x);
+double longitud(int y);
+void calcula_todo();
+void coor();
+void guarda();
+double distancia(int xi, int yi);
+int z = 0;
+int i=0;
+int tierra=0;
+double memoria = 0;
+int map[filas][columnas];
+int xtot[filas*columnas];
+int ytot[filas*columnas];
+int xnow = 350;
+int ynow = 100;
+int rx;
+int ry;
+int posx;
+int posy;
+double rr;
 int main(void) {
-  int map[filas][columnas];
-  int xtot[filas*columnas];
-  int ytot[filas*columnas];
-  int xnow = 265; 
-  int ynow = 70; 
-  int rx; 
-  int ry; 
-  int posx; 
-  int posy; 
-  double rr;
+
   FILE *datos;
   datos=fopen("map_data.txt","r");
-  int tierra=0;
+
   int i = 0;
   int j = 0;
-  int k = 0;
+  int z = 0;
   if (datos){
     const size_t size=10000;
-    char *linea= malloc(size);
+    char* linea= malloc(size);
     while (fgets(linea, size, datos) != NULL){
-      if(j<k){
-        k=j;
+      if(j<z){
+        z=j;
       }
       j=0;
-      char *first=strtok(linea," ");
+      char *first = strtok(linea," ");
       while(first!=NULL){
         double step=strtod(first,NULL);
         map[i][j]=step;
@@ -50,9 +60,66 @@ int main(void) {
 fclose(datos);
 
   }
+    calcula_todo();
+
+    guarda();
 
 
   return 0;
+}
+void calcula_todo(){
+double memoria = 0;
+int recorrido = 2000;
+i = 0;
+
+double despues = 0;
+double antes = 0;
+while(i<recorrido){
+
+
+             coor();
+      despues = distancia(posx,posy);
+      antes = distancia(xnow,ynow);
+        double c = exp(despues - antes);
+
+
+               if(despues>rr){
+                        rx = posx;
+                        ry = posy;
+                        rr = despues;
+                                  }
+               if(c > 1){
+                        c = 1;
+                        }
+               memoria = (double) rand()/RAND_MAX;
+               if(c>memoria){
+                             xnow = posx;
+                             ynow = posy;
+
+                             }
+
+
+i = i+1;
+      }
+}
+void guarda(){
+  FILE *apy =fopen("datos_obtenidos.txt","w+");
+  fprintf(apy,"%d %d %.2f",rx, ry, rr);
+
+
+  printf("Las coordenadas del punto mas alehado son: %.3f %.3f\n", longitud(ynow), latitud(xnow));
+
+
+}
+double latitud(int x){
+       double x1 = (double) x;
+       double res = (-90.0/250)*x1 + 90.0;
+       return res;
+}
+double longitud(int y){
+       double y1 = (double) y;
+       double res = (360.0/744.0)*y1 - 180.0;
+       return res;
 }
 void coor(){
 double x2= ((double)rand()/RAND_MAX);
@@ -79,3 +146,15 @@ else{
         }
 
      }
+double distancia(int xi, int yi){
+  double respuesta = (double) RAND_MAX;
+  for(z = 0;z<tierra;z++){
+     memoria = sqrt( pow(((double) xi- (double) xtot[z]),2) + pow(((double) yi-(double) ytot[z]),2));
+
+    if(memoria<respuesta){
+       respuesta = memoria;
+            }
+
+        }
+       return respuesta;
+  }
